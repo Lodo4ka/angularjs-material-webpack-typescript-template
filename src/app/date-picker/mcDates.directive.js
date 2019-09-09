c388cca609b2a0503d16b258dd5c637eed744982
Lodo4ka <doomymax@gmail.com>
2019-09-10 00:24:38 +0300

    add new changes

---
+++ src/app/date-picker/mcDates.directive.js
@@ -0,0 +1,35 @@
+// export class TableRowBSGroupCtrl {
+//
+//     // Dependency Injection
+//     static $inject: [string] = [
+//         '$scope'
+//         // Apply all the dependancies for the component here
+//         // $scope as an example
+//     ];
+//
+//     // Access Bindings
+//     protected dirvm: any;
+//     protected grouplvl: any;
+//     protected classlvl: any;
+//
+//     constructor(private $scope: ng.IScope) {
+//         // Place the Logics here which are in the controller function in your directive
+//
+//     }
+//
+//     public static factory() {
+//         // Here goes your static function body
+//     }
+//
+// }
+//
+// const options = {
+//     templateUrl: //the path for the template,
+//     bindings: {
+//     dirvm: '=',
+//         grouplvl: '=',
+//         classlvl: '@'
+// },
+// controller: TableRowBSGroupCtrl
+// };
+//# sourceMappingURL=mcDates.directive.js.map
